6a2a245a1b7e219ddda56635e2235dc7fa710793
danvasem <turok3000@gmail.com>
2022-03-13 20:22:15 -0500

    Versión con conexión a Smart Contract

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ Install dependencies:
 - yarn add react-router-dom
 - yarn add @web3-react/core web3-react
 - yarn add web3
-- yarn add @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^6
+- yarn add @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^6 @chakra-ui/icons
+- yarn add @web3-react/injected-connector
 
 There is a known issue using Web3 when the app was created using Creat-react-app, follow the instructions detailed in:
 https://github.com/ChainSafe/web3.js#troubleshooting-and-known-issues
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,7 +1,28 @@
+import { useWeb3React } from "@web3-react/core";
+import { useEffect } from "react";
+import { useCallback } from "react";
+import { useState } from "react";
+import usePlatziPunks from "../../hooks/usePlatziPunks";
+
 const Home = () => {
+  const { active } = useWeb3React();
+  const [maxSupply, setMaxSupply] = useState();
+  const platziPunks = usePlatziPunks();
+  const getMaxSupply = useCallback(async () => {
+    if (platziPunks) {
+      const result = await platziPunks.methods.maxSupply().call();
+      setMaxSupply(result);
+    }
+  }, [platziPunks]);
+
+  useEffect(() => {
+    getMaxSupply();
+  }, [getMaxSupply]);
+
+  if (!active) return "Conecta tu wallet!";
   return (
     <div>
-      <p>Hello world 2</p>
+      <p>Max Supply: {maxSupply}</p>
     </div>
   );
 };
